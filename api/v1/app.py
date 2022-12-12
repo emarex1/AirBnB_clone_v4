b7c319b5475ec491f78294d999d0e91d7b4df0b2
Alatise Oluwaseun <holynation667@gmail.com>
2022-12-13 00:52:55 +0100

    feat: Added status badge to app and check CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,7 +24,7 @@ port = os.getenv('HBNB_API_PORT', 5000)
 # Cross-Origin Resource Sharing
 # cors = CORS(app, origins="0.0.0.0")
 # cors = CORS(app, resources={r'/*': {'origins': host}})
-# cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 # app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
